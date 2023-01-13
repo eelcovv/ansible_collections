2370fb07aa52bb8c76fdee30a6867d26fb2d215d
Eelco van Vliet <eelcovv@gmail.com>
2023-01-13 09:37:26 +0000

    added comment

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 # login as root and run. Do not use sudo as your environment variables are lost
-ansible-playbook -i eelcovv/ldx_webdev_server/plugins/inventory/inventory.yml all.yml --ask-vault-password
+# first source the environment file:
+# source set_vault_password_env.sh
+ansible-playbook -i collections/ansible_collections/eelcovv/ldx_webdev_server/plugins/inventory/inventory.yml all.yml 
 
