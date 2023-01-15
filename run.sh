d7e5605479d4da66b1d14dc8ff4f39a74bc1437d
Eelco van Vliet <eelcovv@gmail.com>
2023-01-15 14:42:26 +0000

    split up playbooks

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # login as root and run. Do not use sudo as your environment variables are lost
 # first source the environment file:
 # source set_vault_password_env.sh
-ansible-playbook -i collections/ansible_collections/eelcovv/ldx_webdev_server/plugins/inventory/inventory.yml all.yml 
+ansible-playbook -i collections/ansible_collections/eelcovv/ldx_webdev_server/plugins/inventory/inventory.yml main.yml 
 
